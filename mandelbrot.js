f3aa312524c435f943eb349b5d2d2bb17b78f8bd
Brian Payne <bpayne@uci.edu>
2016-08-27 23:25:50 -0700

    Made interactive

--- mandelbrot.js
+++ mandelbrot.js
@@ -61,38 +61,46 @@ function Transformer(rasterSize) {
         return normal;
     };
 }
-window.onload = function() {
-    function setPixel(imageData, x, y, r, g, b, a) {
-        index = (x + y * imageData.width) * 4;
-        imageData.data[index+0] = r;
-        imageData.data[index+1] = g;
-        imageData.data[index+2] = b;
-        imageData.data[index+3] = a;
-    }
-    var element = document.getElementById('canvas1');
-    var canvas = document.getElementById('canvas1').getContext('2d');
-    var imageData = canvas.createImageData(element.width, element.height);
-    var rasterSize = {width: element.width, height: element.height};
-    var raster = [];
-    for(var i = 0; i < rasterSize.width; i++) {
-        for(var j = 0; j < rasterSize.height; j++) {
-            var complex = new Complex(0,0);
-            raster.push(new Coord(i, j, complex, 0));
+window.onload = function () {
+    document.getElementsById('go').onclick = function() {
+        function setPixel(imageData, x, y, r, g, b, a) {
+            index = (x + y * imageData.width) * 4;
+            imageData.data[index+0] = r;
+            imageData.data[index+1] = g;
+            imageData.data[index+2] = b;
+            imageData.data[index+3] = a;
         }
-    }
-    var transformer = new Transformer(rasterSize);
-    var normals = transformer.forward(raster);
-    normals.forEach(function(entry) {
-        entry.c.real = entry.x;
-        entry.c.im = entry.y;
-    });
-    var max = 20;
-    var result = iterate(normals, max);
-    raster = transformer.reverse(result);
-    raster.forEach(function(entry) {
-        var intensity = (entry.i/max)*255;
-        setPixel(imageData, entry.x, entry.y, intensity, 0, 0, 255);
-    });
-    canvas.putImageData(imageData,0,0);
+        var element = document.getElementById('canvas1');
+        var canvas = document.getElementById('canvas1').getContext('2d');
+        var imageData = canvas.createImageData(element.width, element.height);
+        var rasterSize = {width: element.width, height: element.height};
+        var raster = [];
+        for(var i = 0; i < rasterSize.width; i++) {
+            for(var j = 0; j < rasterSize.height; j++) {
+                var complex = new Complex(0,0);
+                raster.push(new Coord(i, j, complex, 0));
+            }
+        }
+        var transformer = new Transformer(rasterSize);
+        var normals = transformer.forward(raster);
+
+        var scale = parseFloat(document.getElementById('scale').value) || 1;
+        var xOffset = parseFloat(document.getElementById('xoffset').value) || 0;
+        var yOffset = parseFloat(document.getElementById('yoffset').value) || 0;
+
+        normals.forEach(function(entry) {
+            entry.c.real = entry.x/scale+xOffset;
+            entry.c.im = entry.y/scale+yOffset;
+        });
+        var max = 70;
+        var result = iterate(normals, max);
+        raster = transformer.reverse(result);
+        raster.forEach(function(entry) {
+            var intensity = (entry.i/max)*255;
+            setPixel(imageData, entry.x, entry.y, intensity, 0, 0, 255);
+        });
+        canvas.putImageData(imageData,0,0);
+    };
+
 };
 
